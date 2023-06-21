63a56cb7f89edec7d76160c0dc3f9239a2f5aedf
Jan Neumann <jan.alexander.neumann@gmail.com>
2023-06-21 10:00:24 +0200

    Initial Commit

---
+++ ZenKen/ZenKenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenKenApp.swift
+//  ZenKen
+//
+//  Created by Jan Alexander Neumann on 21.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ZenKenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
